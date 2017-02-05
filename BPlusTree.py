940d930957e6f32c11ef50bc257811a9b5e1a6ac
Jake Myers <jake@directedgemedia.com>
2017-02-04 17:56:45 -0800

    Test coverage for InternalNode

---
+++ BPlusTree.py
@@ -0,0 +1,174 @@
+class Record(object):
+    def __init__(self, key, value):
+        self.key = key
+        self.value = value
+
+
+class InternalNode(object):
+    def __init__(self, size):
+        self.size = size
+        self.keys = [None] * size
+        self.children = [None] * (size + 1)
+        self.record_index = -1
+        self.parent = None
+
+    def full(self):
+        return self.record_index + 1 == self.size - 1
+
+    def next_child(self, key):
+        # Binary search
+        lo = 0
+        hi = self.record_index
+
+        while lo < hi:
+            mid = (lo + hi) // 2
+
+            if key < self.keys[mid]:
+                hi = mid
+            else:
+                lo = mid + 1
+
+        return self.children[lo]
+
+    def insert(self, node, split_key, split_node):
+        # Add the new key-value to the next free spot
+        self.record_index += 1
+        self.keys[self.record_index] = split_key
+        self.children[self.record_index + 1] = split_node
+
+        # Shift the new key-value into sorted order
+        i = self.record_index
+        while i > 0 and self.keys[i] < self.keys[i - 1]:
+            self.keys[i], self.keys[i - 1] = self.keys[i - 1], self.keys[i]
+            self.children[i + 1], self.children[i] = self.children[i], self.children[i + 1]
+            i -= 1
+
+        self.children[i] = node
+
+    def split(self):
+        node = self.__class__(self.size)
+        node.parent = self.parent
+
+        # Partition records between the two nodes
+        copy_position = self.size // 2
+        for i in range(copy_position + 1, self.size):
+            node.insert(self.children[i], self.keys[i], self.children[i + 1])
+
+        # Store the split key before we remove it from the node
+        split_key = self.keys[copy_position]
+
+        # Remove records from node that were copied to the new node
+        for i in range(copy_position, self.size):
+            self.keys[i] = None
+            self.children[i + 1] = None
+
+        self.record_index = copy_position - 1
+
+        return split_key, node
+
+
+class LeafNode(object):
+    def __init__(self, size):
+        self.size = size
+        self.keys = [None] * size
+        self.records = [None] * size
+        self.next_leaf_node = None
+        self.record_index = -1
+        self.parent = None
+
+    def full(self):
+        return self.record_index + 1 == self.size - 1
+
+    def insert(self, key, value):
+        # Add the new key-value to the next free spot
+        self.record_index += 1
+        self.keys[self.record_index] = key
+        self.records[self.record_index] = Record(key, value)
+
+        # Shift the new key-value into sorted order
+        i = self.record_index
+        while i > 0 and self.keys[i] < self.keys[i - 1]:
+            self.keys[i], self.keys[i - 1] = self.keys[i - 1], self.keys[i]
+            self.records[i], self.records[i - 1] = self.records[i - 1], self.records[i]
+            i -= 1
+
+    def split(self):
+        # Create a new empty leaf node
+        node = self.__class__(self.size)
+        node.parent = self.parent
+
+        # Update next_leaf_node pointers
+        node.next_leaf_node = self.next_leaf_node
+        self.next_leaf_node = node
+
+        # Partition records between the two nodes
+        copy_position = self.size // 2
+        for i in range(copy_position, self.size):
+            node.insert(self.keys[i], self.records[i])
+            self.keys[i] = None
+            self.records[i] = None
+
+        self.record_index = copy_position - 1
+
+        return node.keys[0], node
+
+
+class BPlusTree(object):
+    def __init__(self, size):
+        self.size = size
+        self.root = None
+
+    def _find_leaf(self, key, root):
+        node = root
+        while type(node) is not LeafNode:
+            node = node.next_child(key)
+        return node
+
+    def insert_in_parent(self, node, split_key, split_node):
+        if node is self.root:
+            internal_node = InternalNode(self.size)
+            internal_node.insert(node, split_key, split_node)
+            self.root = internal_node
+            node.parent = internal_node
+            split_node.parent = internal_node
+        else:
+            parent = node.parent
+
+            if not parent.full():
+                parent.insert(split_key, node, split_node)
+            else:
+                parent.insert(split_key, node, split_node)
+                split_key, split_parent = parent.split()
+                self.insert_in_parent(parent, split_key, split_parent)
+
+    def __setitem__(self, key, value):
+        self.set(key, value)
+
+    def set(self, key, value):
+        if not self.root:
+            # If the tree is empty, create a new LeafNode and set it to root
+            self.root = LeafNode(self.size)
+            node = self.root
+        else:
+            # Find the LeafNode that should contain key
+            node = self._find_leaf(key, self.root)
+
+        if not node.full():
+            # If the node has room, then insert the key-value pair
+            node.insert(key, value)
+        else:
+            # If the node is full, insert then split
+            node.insert(key, value)
+            split_key, split_node = node.split()
+
+            # Insert the 2 nodes into parent internal nodes
+            self.insert_in_parent(node, split_key, split_node)
+
+    def get(self, key):
+        pass
+
+    def delete(self, key):
+        pass
+
+    def exists(self, key):
+        pass
--- test/test_internal_node.py
+++ test/test_internal_node.py
@@ -1,35 +1,106 @@
 import unittest
 from BPlusTree import InternalNode
 
+
 class TestInternalNode(unittest.TestCase):
-	
-	def test_key_value_size(self):
-		size = 10
-		node = InternalNode(size)
-		
-		self.assertEqual(size, len(node.keys))
-		self.assertEqual(size+1, len(node.children))
-	
-	def test_next_child(self):
-		size = 10
-		node = InternalNode(size)
-		node.keys = ['c', 'd', 'e', 'f', 'h', 'i', 'j', 'k', 'l', 'm']
-		node.children = [i for i in range(len(node.children))]
-		
-		# Low Key Out-of-Range
-		self.assertEqual(0, node.next_child("a"))
-		
-		# Smallest Key
-		self.assertEqual(0, node.next_child("c"))
-		
-		# Middle Key - Exists
-		self.assertEqual(5, node.next_child("i"))
-		
-		# Middle Key - Not Exists
-		self.assertEqual(4, node.next_child("g"))
-		
-		# Largest Key
-		self.assertEqual(9, node.next_child("m"))
-		
-		# High Key Out-of-Range
-		self.assertEqual(10, node.next_child("p"))
+    def test_key_value_size(self):
+        size = 10
+        node = InternalNode(size)
+
+        self.assertEqual(size, len(node.keys))
+        self.assertEqual(size + 1, len(node.children))
+
+    def test_full(self):
+        size = 4
+        node = InternalNode(size)
+
+        cases = ['a', 'b']
+
+        for case in cases:
+            node.insert(None, case, None)
+            self.assertFalse(node.full())
+
+        node.insert(None, 'c', None)
+        self.assertTrue(node.full())
+
+
+    def test_next_child(self):
+        size = 10
+        node = InternalNode(size)
+
+        node.record_index = 10
+        node.keys = ['c', 'd', 'e', 'f', 'h', 'i', 'j', 'k', 'l', 'm']
+        node.children = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+        # Low Key Out-of-Range
+        self.assertEqual(0, node.next_child("a"))
+
+        # Smallest Key
+        self.assertEqual(1, node.next_child("c"))
+
+        # Middle Key - Exists
+        self.assertEqual(6, node.next_child("i"))
+
+        # Middle Key - Not Exists
+        self.assertEqual(4, node.next_child("g"))
+
+        # Largest Key
+        self.assertEqual(10, node.next_child("m"))
+
+        # High Key Out-of-Range
+        self.assertEqual(10, node.next_child("p"))
+
+    def test_insert(self):
+        size = 3
+        node = InternalNode(size)
+
+        node.insert(3, 'c', 4)
+        self.assertEqual(node.keys, ['c', None, None])
+        self.assertEqual(node.children, [3, 4, None, None])
+
+        node.insert(5, 'e', 6)
+        self.assertEqual(node.keys, ['c', 'e', None])
+        self.assertEqual(node.children, [3, 5, 6, None])
+
+        node.insert(1, 'a', 2)
+        self.assertEqual(node.keys, ['a', 'c', 'e'])
+        self.assertEqual(node.children, [1, 2, 5, 6])
+
+    def test_split_odd(self):
+        size = 5
+        node = InternalNode(size)
+
+        node.insert(1, 'a', 2)
+        node.insert(2, 'b', 3)
+        node.insert(3, 'c', 4)
+        node.insert(4, 'd', 5)
+        node.insert(5, 'e', 6)
+
+        split_key, split_node = node.split()
+
+        self.assertEqual(split_key, 'c')
+        self.assertEqual(node.keys, ['a', 'b', None, None, None])
+        self.assertEqual(node.children, [1, 2, 3, None, None, None])
+        self.assertEqual(node.record_index, 1)
+
+        self.assertEqual(split_node.keys, ['d', 'e', None, None, None])
+        self.assertEqual(split_node.children, [4, 5, 6, None, None, None])
+
+    def test_split_even(self):
+        size = 4
+        node = InternalNode(size)
+
+        node.insert(1, 'a', 2)
+        node.insert(2, 'b', 3)
+        node.insert(3, 'c', 4)
+        node.insert(4, 'd', 5)
+
+        split_key, split_node = node.split()
+
+        self.assertEqual(split_key, 'c')
+        self.assertEqual(node.keys, ['a', 'b', None, None])
+        self.assertEqual(node.children, [1, 2, 3, None, None])
+        self.assertEqual(node.record_index, 1)
+
+        self.assertEqual(split_node.keys, ['d', None, None, None])
+        self.assertEqual(split_node.children, [4, 5, None, None, None])
